78fc6d3759541fa1652b346fc1d0d9040941aa24
Alex Skrypnyk <alex@drevops.com>
2026-09-05 13:08:05 +1000

    Named the converter benchmark subjects as format-qualified constants.

--- benchmarks/ConverterBenchmark.php
+++ benchmarks/ConverterBenchmark.php
@@ -25,6 +25,21 @@ class ConverterBenchmark extends AbstractBenchmark {
   protected const CAMEL_INPUT = 'aSampleFieldLabel42WithUnicodeValue';
 
   /**
+   * A snake_case input for the snake-origin converters.
+   */
+  protected const SNAKE_INPUT = 'a_sample_field_label_42_with_unicode_value';
+
+  /**
+   * A kebab-case input for the kebab-origin converters.
+   */
+  protected const KEBAB_INPUT = 'a-sample-field-label-42-with-unicode-value';
+
+  /**
+   * A COBOL-CASE input for the cobol-origin converters.
+   */
+  protected const COBOL_INPUT = 'A-SAMPLE-FIELD-LABEL-42-WITH-UNICODE-VALUE';
+
+  /**
    * Benchmark converting camelCase to snake_case.
    */
   public function benchCamelToSnake(): void {
@@ -42,21 +57,21 @@ class ConverterBenchmark extends AbstractBenchmark {
    * Benchmark converting snake_case to camelCase.
    */
   public function benchSnakeToCamel(): void {
-    Str2Name::snake2camel('a_sample_field_label_42_with_unicode_value');
+    Str2Name::snake2camel(self::SNAKE_INPUT);
   }
 
   /**
    * Benchmark converting kebab-case to PascalCase.
    */
   public function benchKebabToPascal(): void {
-    Str2Name::kebab2pascal('a-sample-field-label-42-with-unicode-value');
+    Str2Name::kebab2pascal(self::KEBAB_INPUT);
   }
 
   /**
    * Benchmark converting COBOL-CASE to PascalCase.
    */
   public function benchCobolToPascal(): void {
-    Str2Name::cobol2pascal('A-SAMPLE-FIELD-LABEL-42-WITH-UNICODE-VALUE');
+    Str2Name::cobol2pascal(self::COBOL_INPUT);
   }
 
 }
